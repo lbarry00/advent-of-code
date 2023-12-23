a906e70a2cc6bbedcccc1b8886733126e85c6e7b
Leo Barry <github.q56mq@simplelogin.com>
2023-12-22 23:52:45 -0800

    Added actual solution for day 5 part 1

--- 2023/05/solution.py
+++ 2023/05/solution.py
@@ -17,47 +17,47 @@ LIGHT_TO_TEMPERATURE_START = 160
 TEMPERATURE_TO_HUMIDITY_START = 196
 HUMIDITY_TO_LOCATION_START = 243
 
+MAPS = [SEED_TO_SOIL_START,
+    SOIL_TO_FERTILIZER_START,
+    FERTILIZER_TO_WATER_START,
+    WATER_TO_LIGHT_START,
+    LIGHT_TO_TEMPERATURE_START,
+    TEMPERATURE_TO_HUMIDITY_START,
+    HUMIDITY_TO_LOCATION_START]
+
 def solution_one(input):
     min_location = sys.maxsize
     seeds = input[0].split(":")[1].strip().split(" ")
-
-    mappings_list = []
-    mappings_list.append(__build_map(SEED_TO_SOIL_START, input))
-    mappings_list.append(__build_map(SOIL_TO_FERTILIZER_START, input))
-    mappings_list.append(__build_map(FERTILIZER_TO_WATER_START, input))
-    mappings_list.append(__build_map(WATER_TO_LIGHT_START, input))
-    mappings_list.append(__build_map(LIGHT_TO_TEMPERATURE_START, input))
-    mappings_list.append(__build_map(TEMPERATURE_TO_HUMIDITY_START, input))
-    mappings_list.append(__build_map(HUMIDITY_TO_LOCATION_START, input))
-
-    # apply each map
-    items = seeds
-    for map in mappings_list:
-        for i in range(0, len(items)):
-            seed = int(items[i])
-            if seed in map:
-                items[i] = map[seed]
-            else: pass
-
-    # look for smallest location value
-    for loc in items:
-        if int(loc) < min_location:
-            min_location = int(loc)
-
+    
+    for seed in seeds:
+        seed = int(seed)
+        for map in MAPS:
+            seed = __apply_map(seed, map, input)
+        # after applying all maps, we end up with location, so find the min
+        if seed < min_location:
+            min_location = seed
     return min_location
 
-def __build_map(map_line_start, input):
-    map = {}
+def __apply_map(seed, map_line_start, input):
     for i in range(map_line_start, len(input)):
         line = input[i]
-        if not line: break
-        nums = line.split(" ")
-        dest_start = nums[0]
-        source_start = nums[1]
-        range_length = nums[2]
-        for j in range(0, int(range_length)):
-            map[int(source_start) + j] = int(dest_start) + j
-    return map
+        split = line.split(" ")
+        if not line or not split[0].isnumeric():
+                break
+        
+        dest_start = int(split[0])
+        source_start = int(split[1])
+        range_length = int(split[2])
+        source_range = range(source_start, source_start+range_length)
+        if seed in source_range:
+            # apply transform via mapping
+            old = seed
+            transform = seed - source_start
+            seed = dest_start + transform
+            #print(str(old) + "\t->\t" + str(seed))
+            # only transform the seed ONCE per mapping
+            break
+    return seed
 
 def solution_two(input):
     return 0
